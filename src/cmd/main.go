0f3246a2174b8a5f917734ff502cd92b5119d8fc
yqguodataman <yqguo@dataman-inc.com>
2017-01-06 18:24:25 +0800

    update

---
+++ src/cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Dataman-Cloud/hamal/src/cmd/command"
+
+	//log "github.com/Sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	hamal := cli.NewApp()
+	hamal.Name = "hamal cl"
+	hamal.Usage = "dataman hamal command-line"
+	hamal.Version = "0.1"
+
+	hamal.Commands = []cli.Command{
+		command.NewCreateCommand(),
+	}
+	hamal.Run(os.Args)
+}
