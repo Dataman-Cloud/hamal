822e721938b675b8e53fae0439c83581c9c417fd
Weitao Zhou <wtzhou@dataman-inc.com>
2017-01-11 16:41:48 +0800

    Interactive

--- src/hamalcli/command/deploy.go
+++ src/hamalcli/command/deploy.go
@@ -5,12 +5,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/Dataman-Cloud/hamal/src/models"
+	ui "github.com/gizak/termui"
+	"github.com/urfave/cli"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/Dataman-Cloud/hamal/src/models"
-
-	"github.com/urfave/cli"
+	"strconv"
 	"strings"
 )
 
@@ -64,7 +64,9 @@ func DeployAction(c *cli.Context) error {
 		if err != nil {
 			return cli.NewExitError(fmt.Sprintf("%s", err.Error()), 1)
 		}
-		rollingUpdateProject(project)
+		if confirmRollingUpdate(project) {
+			rollingUpdateProject(project)
+		}
 	}
 	return nil
 }
@@ -141,4 +143,109 @@ func rollingUpdateProject(project *models.Project) error {
 		}
 	}
 	return nil
+}
+
+func confirmRollingUpdate(project *models.Project) bool {
+	if err := ui.Init(); err != nil {
+		panic(err)
+	}
+	defer ui.Close()
+
+	header := ui.NewPar("Press q to quit, Press h/<- , l/-> to switch options, Press enter to excute the option")
+	header.Height = 2
+	header.Width = 50
+	header.Border = false
+	header.TextBgColor = ui.ColorBlue
+
+	// TODO (wtzhou) only support per-app-per-project
+	stagesSum := len(project.Applications[0].RollingUpdatePolicy)
+	currentStage := int(project.Applications[0].CurrentStage)
+	stagesArray := make([]string, stagesSum)
+
+	for i := 0; i < int(currentStage); i++ {
+		stagesArray[i] = " [" + strconv.Itoa(i) + "] " + "[Updated " + strconv.Itoa(int(project.Applications[0].RollingUpdatePolicy[i].InstancesToUpdate)) + " instances](fg-blue)"
+	}
+	stagesArray[currentStage] = "*[" + strconv.Itoa(currentStage) + "] " + "[Pending update " + strconv.Itoa(int(project.Applications[0].RollingUpdatePolicy[currentStage].InstancesToUpdate)) + " instances](fg-white,bg-green)"
+	for i := int(currentStage) + 1; i < stagesSum; i++ {
+		stagesArray[i] = " [" + strconv.Itoa(i) + "] " + "[Pending update " + strconv.Itoa(int(project.Applications[0].RollingUpdatePolicy[i].InstancesToUpdate)) + " instances](fg-gray)"
+	}
+
+	stagesUI := ui.NewList()
+	stagesUI.Items = stagesArray
+	stagesUI.ItemFgColor = ui.ColorYellow
+	stagesUI.BorderLabel = project.Name + " Progress..."
+	stagesUI.Height = 10
+	stagesUI.Width = 20
+
+	confirmBar := ui.NewPar("Confirm")
+	confirmBar.TextFgColor = ui.ColorWhite
+	confirmBar.TextBgColor = ui.ColorGreen
+	confirmBar.Height = 2
+	confirmBar.Width = 5
+	confirmBar.Border = false
+
+	cancelBar := ui.NewPar("Cancel")
+	cancelBar.Height = 2
+	cancelBar.TextFgColor = ui.ColorWhite
+	cancelBar.TextBgColor = ui.ColorDefault
+	cancelBar.Width = 5
+	cancelBar.Border = false
+
+	ui.Body.AddRows(
+		ui.NewRow(
+			ui.NewCol(3, 2, header),
+		),
+		ui.NewRow(
+			ui.NewCol(4, 2, stagesUI),
+		),
+		ui.NewRow(
+			ui.NewCol(2, 2, cancelBar),
+			ui.NewCol(1, 1, confirmBar),
+		),
+	)
+
+	ui.Body.Y = 3
+	ui.Body.Align()
+	ui.Render(ui.Body)
+
+	confirm := true
+
+	ui.Handle("/sys/kbd/q", func(ui.Event) {
+		ui.StopLoop()
+		confirm = false
+	})
+	ui.Handle("/sys/kbd/h", func(ui.Event) {
+		highlightToggle(cancelBar, confirmBar)
+		ui.Render(ui.Body)
+		confirm = false
+	})
+	ui.Handle("/sys/kbd/l", func(ui.Event) {
+		highlightToggle(confirmBar, cancelBar)
+		ui.Render(ui.Body)
+		confirm = true
+	})
+	ui.Handle("/sys/kbd/<left>", func(ui.Event) {
+		highlightToggle(cancelBar, confirmBar)
+		ui.Clear()
+		ui.Render(ui.Body)
+		confirm = false
+	})
+	ui.Handle("/sys/kbd/<right>", func(ui.Event) {
+		highlightToggle(confirmBar, cancelBar)
+		ui.Clear()
+		ui.Render(ui.Body)
+		confirm = true
+	})
+	ui.Handle("/sys/kbd/<enter>", func(ui.Event) {
+		ui.StopLoop()
+	})
+	ui.Loop()
+	return confirm
+}
+
+func highlightToggle(parA *ui.Par, parB *ui.Par) {
+	parA.TextFgColor = ui.ColorWhite
+	parA.TextBgColor = ui.ColorGreen
+	parB.TextFgColor = ui.ColorWhite
+	parB.TextBgColor = ui.ColorDefault
 }
